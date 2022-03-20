d11aadaa9356f37734259d43797e89fab7e61005
Reinhardt <notmarx.tech@gmail.com>
2022-03-20 15:57:07 +0700

    Create guild data on `ready` event

--- framework/lib/events/ready.ts
+++ framework/lib/events/ready.ts
@@ -1,13 +1,38 @@
 import { Event } from "../interfaces";
+import { GuildModel } from "../models";
 import { Logger } from "../util";
+import { HikariMongoModel } from "../commands";
 
 export const event: Event = {
     name: "ready",
     run: (client) => {
         const logger = new Logger();
+        const guilds = client.guilds.map((guild) => guild.id);
 
         if (client.ready) {
-            logger.info({ type: "INFO", title: "CLIENT READY", subTitle: "HikariFramework::Gateway", message: `${client.user.username}#${client.user.discriminator} Connected` });
+            logger.info({ message: `${client.user.username}#${client.user.discriminator} Connected`, subTitle: "HikariFramework::Gateway", title: "CLIENT READY", type: "INFO" });
+        }
+
+        if (client.database) {
+            logger.info({ message: "Connected To Database", subTitle: "HikariFramework::Database", title: "MONGODB", type: "INFO" });
+        } else {
+            logger.error({ message: "Unable To Connect To Database", subTitle: "HikariFramework::Database", title: "MONGODB", type: "ERROR" });
+        }
+
+        // Create database for guild
+        for (let i = 0; i < guilds.length; i++) {
+            const model = GuildModel.createModel(client.database);
+
+            if (model.findOne({ id: guilds[i] } )) {
+                HikariMongoModel.createGuildModel(client.database, {
+                    createdAt: new Date(),
+                    id: guilds[i],
+                    name: client.guilds.get(guilds[i]).name,
+                    settings: {
+                        prefix: client.config.BOT.PREFIX
+                    }
+                });
+            }
         }
     }
 };
